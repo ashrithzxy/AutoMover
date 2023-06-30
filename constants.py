10a15653e70e384f493a63482d7a4c17b6446b96
ashrithsuresh <ashrithx@gmail.com>
2023-06-30 18:05:08 +0530

    Removed unnecessary constant

--- constants.py
+++ constants.py
@@ -4,5 +4,4 @@ SNAPSEED_MEDIA_BASEPATH = "/sdcard/Snapseed/"
 DCIM_MEDIA_BASEPATH = "/sdcard/DCIM/Camera/"
 PICTURES_MEDIA_BASEPATH = "/sdcard/Pictures/"
 BACKUP_COMMAND = 'adb shell find "{}" -type f | grep "{}" | xargs -I {} adb pull {} {}'
-ADB_TEST = "My name is {}, I am a {}, I am {} {} years old {}."
 MOTOG6 = "ZF62226S37"
